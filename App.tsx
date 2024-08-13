2d703211c4ceff85f1237ab85cdde3e927fe6344
meryem achouri <meryem@gmail.com>
2024-08-13 11:31:20 +0100

    last fix

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FormScreen from './app/screens/FormScreen'
+import { Provider } from 'react-redux'
+import { store } from './app/store/store'
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
      <FormScreen/>
-    </>
+    </Provider>
   )
 }
 
--- index.js
+++ index.js
@@ -5,13 +5,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { Provider } from 'react-redux';
-import { store } from './app/store/store';
 
-const AppWithProvider = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-AppRegistry.registerComponent(appName, () => AppWithProvider);
+AppRegistry.registerComponent(appName, () => App);
